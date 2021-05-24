336c50ebd62c7e1c83068ffe9cf8650ef7a14d58
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-05-24 10:44:26 +0800

    feat: lab3s testbench passed
    Signed-off-by: ffyuanda <46557895+ffyuanda@users.noreply.github.com>

---
+++ Lab_3_Accelerating_Object_Locator_Structural/Lab_3_Accelerating_Object_Locator_Structural.srcs/sources_1/imports/Downloads/lab3s.vhd
@@ -0,0 +1,522 @@
+----------------------------------------------------------------------
+-- EECS31L Assignment 3
+-- Locator Structural Model
+----------------------------------------------------------------------
+-- Student First Name : Shaoxuan
+-- Student Last Name : Yuan
+-- Student ID : 89832399
+----------------------------------------------------------------------
+
+---------- Components library ----------
+
+---------- 8x16 Register File ----------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+ENTITY RegFile IS
+   PORT (R_Addr1, R_Addr2, W_Addr: IN std_logic_vector(2 DOWNTO 0);
+         R_en, W_en: IN std_logic;
+         Reg_Data1 : OUT std_logic_vector(15 DOWNTO 0); 
+			Reg_Data2 : OUT std_logic_vector(15 DOWNTO 0); 
+         W_Data: IN std_logic_vector(15 DOWNTO 0); 
+         Clk, Rst: IN std_logic);
+END RegFile;
+
+ARCHITECTURE Beh OF RegFile IS 
+   TYPE regArray_type IS 
+      ARRAY (0 TO 7) OF std_logic_vector(15 DOWNTO 0); 
+   SIGNAL regArray : regArray_type;
+BEGIN
+   WriteProcess: PROCESS(Clk)    
+   BEGIN
+      IF (Clk = '1' AND Clk'EVENT) THEN
+         IF (Rst = '1') THEN
+            regArray(0) <= X"0000" AFTER 6.0 NS;
+            regArray(1) <= X"000B" AFTER 6.0 NS;
+            regArray(2) <= X"0023" AFTER 6.0 NS;
+            regArray(3) <= X"0007" AFTER 6.0 NS;
+            regArray(4) <= X"000A" AFTER 6.0 NS;
+            regArray(5) <= X"0000" AFTER 6.0 NS;
+            regArray(6) <= X"0000" AFTER 6.0 NS;
+            regArray(7) <= X"0000" AFTER 6.0 NS;
+         ELSE
+            IF (W_en = '1') THEN
+                regArray(conv_integer(W_Addr)) <= W_Data AFTER 6.0 NS;
+                END IF;
+        END IF;
+     END IF;
+   END PROCESS;
+            
+   ReadProcess1: PROCESS(R_en, R_Addr1, regArray)
+   BEGIN
+      IF (R_en = '1') THEN
+        CASE R_Addr1 IS
+            WHEN "000" =>
+                Reg_Data1 <= regArray(0) AFTER 6.0 NS;
+            WHEN "001" =>
+                Reg_Data1 <= regArray(1) AFTER 6.0 NS;
+            WHEN "010" =>
+                Reg_Data1 <= regArray(2) AFTER 6.0 NS;
+            WHEN "011" =>
+                Reg_Data1 <= regArray(3) AFTER 6.0 NS;
+            WHEN "100" =>
+                Reg_Data1 <= regArray(4) AFTER 6.0 NS;
+            WHEN "101" =>
+                Reg_Data1 <= regArray(5) AFTER 6.0 NS;
+            WHEN "110" =>
+                Reg_Data1 <= regArray(6) AFTER 6.0 NS;
+            WHEN "111" =>
+                Reg_Data1 <= regArray(7) AFTER 6.0 NS;
+            WHEN OTHERS =>
+                Reg_Data1 <= (OTHERS=>'0') AFTER 6.0 NS;
+        END CASE;
+      ELSE
+        Reg_Data1 <= (OTHERS=>'0') AFTER 6.0 NS;
+      END IF;
+   END PROCESS;
+	
+	ReadProcess2: PROCESS(R_en, R_Addr2, regArray)
+   BEGIN
+      IF (R_en = '1') THEN
+        CASE R_Addr2 IS
+            WHEN "000" =>
+                Reg_Data2 <= regArray(0) AFTER 6.0 NS;
+            WHEN "001" =>
+                Reg_Data2 <= regArray(1) AFTER 6.0 NS;
+            WHEN "010" =>
+                Reg_Data2 <= regArray(2) AFTER 6.0 NS;
+            WHEN "011" =>
+                Reg_Data2 <= regArray(3) AFTER 6.0 NS;
+            WHEN "100" =>
+                Reg_Data2 <= regArray(4) AFTER 6.0 NS;
+            WHEN "101" =>
+                Reg_Data2 <= regArray(5) AFTER 6.0 NS;
+            WHEN "110" =>
+                Reg_Data2 <= regArray(6) AFTER 6.0 NS;
+            WHEN "111" =>
+                Reg_Data2 <= regArray(7) AFTER 6.0 NS;
+            WHEN OTHERS =>
+                Reg_Data2 <= (OTHERS=>'0') AFTER 6.0 NS;
+        END CASE;
+      ELSE
+        Reg_Data2 <= (OTHERS=>'0') AFTER 6.0 NS;
+      END IF;
+   END PROCESS;
+END Beh;
+
+
+---------- 16-Bit ALU ----------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+
+ENTITY ALU IS
+    PORT (Sel: IN std_logic;
+            A: IN std_logic_vector(15 DOWNTO 0);
+            B: IN std_logic_vector(15 DOWNTO 0);
+            ALU_Out: OUT std_logic_vector (15 DOWNTO 0) );
+END ALU;
+
+ARCHITECTURE Beh OF ALU IS
+
+BEGIN
+    PROCESS (A, B, Sel)
+         variable temp: std_logic_vector(31 DOWNTO 0):= X"00000000";
+    BEGIN
+        IF (Sel = '0') THEN
+            ALU_Out <= A + B AFTER 12 NS;                
+        ELSIF (Sel = '1') THEN
+            temp := A * B ;
+                ALU_Out <= temp(15 downto 0) AFTER 12 NS; 
+        END IF;
+          
+    END PROCESS;
+END Beh;
+
+
+---------- 16-bit Shifter ----------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY Shifter IS
+   PORT (I: IN std_logic_vector(15 DOWNTO 0);
+         Q: OUT std_logic_vector(15 DOWNTO 0);
+         sel: IN std_logic );
+END Shifter;
+
+ARCHITECTURE Beh OF Shifter IS 
+BEGIN
+   PROCESS (I,sel) 
+   BEGIN
+         IF (sel = '1') THEN 
+            Q <= I(14 downto 0) & '0' AFTER 4.0 NS;
+         ELSE
+            Q <= '0' & I(15 downto 1) AFTER 4.0 NS;
+         END IF;   
+   END PROCESS; 
+END Beh;
+
+
+---------- 2-to-1 Selector ----------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY Selector IS
+   PORT (sel: IN std_logic;
+         x,y: IN std_logic_vector(15 DOWNTO 0);
+         f: OUT std_logic_vector(15 DOWNTO 0));
+END Selector;
+
+ARCHITECTURE Beh OF Selector IS 
+BEGIN
+   PROCESS (x,y,sel)
+   BEGIN
+         IF (sel = '0') THEN
+            f <= x AFTER 3.0 NS;
+         ELSE
+            f <= y AFTER 3.0 NS;
+         END IF;   
+   END PROCESS; 
+END Beh;
+
+
+---------- 16-bit Register ----------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY Reg IS
+   PORT (I: IN std_logic_vector(15 DOWNTO 0);
+         Q: OUT std_logic_vector(15 DOWNTO 0);
+         Ld: IN std_logic; 
+         Clk, Rst: IN std_logic );
+END Reg;
+
+ARCHITECTURE Beh OF Reg IS 
+BEGIN
+   PROCESS (Clk)
+   BEGIN
+      IF (Clk = '1' AND Clk'EVENT) THEN
+         IF (Rst = '1') THEN
+            Q <= X"0000" AFTER 4.0 NS;
+         ELSIF (Ld = '1') THEN
+            Q <= I AFTER 4.0 NS;
+         END IF;   
+      END IF;
+   END PROCESS; 
+END Beh;
+
+---------- 16-bit Three-state Buffer ----------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY ThreeStateBuff IS
+    PORT (Control_Input: IN std_logic;
+          Data_Input: IN std_logic_vector(15 DOWNTO 0);
+          Output: OUT std_logic_vector(15 DOWNTO 0) );
+END ThreeStateBuff;
+
+ARCHITECTURE Beh OF ThreeStateBuff IS
+BEGIN
+    PROCESS (Control_Input, Data_Input)
+    BEGIN
+        IF (Control_Input = '1') THEN
+            Output <= Data_Input AFTER 2 NS;
+        ELSE
+            Output <= (OTHERS=>'Z') AFTER 2 NS;
+        END IF;
+    END PROCESS;
+END Beh;
+
+---------- Controller ----------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+ENTITY Controller IS
+    PORT(R_en: OUT std_logic;
+         W_en: OUT std_logic;
+         R_Addr1: OUT std_logic_vector(2 DOWNTO 0);
+         R_Addr2: OUT std_logic_vector(2 DOWNTO 0);
+         W_Addr: OUT std_logic_vector(2 DOWNTO 0);
+         Shifter_Sel: OUT std_logic;
+         Selector_Sel: OUT std_logic;
+         ALU_sel : OUT std_logic;
+         OutReg_Ld: OUT std_logic;
+         Oe: OUT std_logic;
+         Done: OUT std_logic;
+         Start, Clk, Rst: IN std_logic); 
+END Controller;
+
+
+ARCHITECTURE Beh OF Controller IS
+    TYPE Statetype IS (S_Start, Comp1, Comp2, Comp3, Comp4, Comp5, Comp6, S_End);
+    SIGNAL Currstate, Nextstate: Statetype;
+BEGIN
+    CombLogic: PROCESS(Clk, Currstate)
+    BEGIN
+        IF (Rst = '1') THEN
+            Nextstate <= S_Start;
+        END IF;
+        
+        CASE Currstate IS
+            WHEN S_Start =>
+                IF (Start = '1') THEN
+                    Nextstate <= Comp1 AFTER 11 NS;
+                END IF;
+                    
+            WHEN Comp1 =>
+                R_en <= '1';
+                R_Addr1 <= "010";
+                R_Addr2 <= "010";
+                W_Addr <= "101";
+                W_en <= '1';
+                ALU_sel <= '1';
+                Selector_Sel <= '1';
+                OutReg_Ld <= '0';
+                Oe <= '0';
+                Nextstate <= Comp2 AFTER 11 NS;
+                
+            WHEN Comp2 =>
+                R_en <= '1';
+                R_Addr1 <= "001";
+                R_Addr2 <= "101";
+                W_Addr <= "101";
+                W_en <= '1';
+                ALU_sel <= '1';
+                Selector_Sel <= '1';
+                OutReg_Ld <= '0';
+                Oe <= '0';
+                Nextstate <= Comp3 AFTER 11 NS;
+                
+            WHEN Comp3 =>
+                R_en <= '1';
+                R_Addr1 <= "001";
+                R_Addr2 <= "101";
+                W_Addr <= "101";
+                W_en <= '1';
+                ALU_sel <= '1';
+                Shifter_Sel <= '0';
+                Selector_Sel <= '0';
+                OutReg_Ld <= '0';
+                Oe <= '0';         
+                Nextstate <= Comp4 AFTER 11 NS;       
+
+            WHEN Comp4 =>
+                R_en <= '1';
+                R_Addr1 <= "010";
+                R_Addr2 <= "011";
+                W_Addr <= "110";
+                W_en <= '1';
+                ALU_sel <= '1';
+                Selector_Sel <= '1';
+                OutReg_Ld <= '0';
+                Oe <= '0';
+                Nextstate <= Comp5 AFTER 11 NS;
+                
+            WHEN Comp5 =>
+                R_en <= '1';
+                R_Addr1 <= "101";
+                R_Addr2 <= "110";
+                W_Addr <= "111";
+                W_en <= '1';
+                ALU_sel <= '0';
+                Selector_Sel <= '1';
+                OutReg_Ld <= '0';
+                Oe <= '0';    
+                Nextstate <= Comp6 AFTER 11 NS;    
+                
+            WHEN Comp6 =>
+                R_en <= '1';
+                R_Addr1 <= "111";
+                R_Addr2 <= "100";
+                W_Addr <= "111";
+                W_en <= '1';
+                ALU_sel <= '0';
+                Selector_Sel <= '1';
+                OutReg_Ld <= '1';                
+                Oe <= '0';
+                Nextstate <= S_END AFTER 11 NS;                     
+                                        
+            WHEN S_End =>
+                W_en <= '0';
+                R_en <= '0';
+                OutReg_Ld <= '0';
+                Done <= '1';
+                Oe <= '1';
+                Nextstate <= S_Start AFTER 11 NS;
+                
+            WHEN OTHERS =>
+        END CASE;
+
+    END PROCESS;
+    
+    StateReg: PROCESS(Clk)
+    BEGIN
+        IF (Clk = '1' AND Clk'EVENT) THEN
+            Currstate <= Nextstate AFTER 4 NS;
+        END IF;
+    END PROCESS;
+    
+
+END Beh;
+
+
+---------- Locator (with clock cycle =  38 NS )----------
+--         INDICATE YOUR CLOCK CYCLE TIME ABOVE      ----
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+entity Locator_struct is
+    Port ( Clk : in  STD_LOGIC;
+		   Start : in  STD_LOGIC;
+           Rst : in  STD_LOGIC;
+           Loc : out  STD_LOGIC_VECTOR (15 downto 0);
+           Done : out  STD_LOGIC);
+end Locator_struct;
+
+architecture Struct of Locator_struct is
+    
+    COMPONENT RegFile IS
+        PORT (  R_Addr1, R_Addr2, W_Addr: IN std_logic_vector(2 DOWNTO 0);
+                R_en, W_en: IN std_logic;
+                Reg_Data1: OUT std_logic_vector(15 DOWNTO 0); 
+				Reg_Data2: OUT std_logic_vector(15 DOWNTO 0);
+                W_Data: IN std_logic_vector(15 DOWNTO 0); 
+                Clk, Rst: IN std_logic );
+    END COMPONENT;
+    
+    COMPONENT ALU IS
+        PORT (Sel: IN std_logic;
+                A: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+                B: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+                ALU_Out: OUT STD_LOGIC_VECTOR (15 DOWNTO 0) );
+    END COMPONENT;
+
+    COMPONENT Shifter IS
+         PORT (I: IN std_logic_vector(15 DOWNTO 0);
+               Q: OUT std_logic_vector(15 DOWNTO 0);
+               sel: IN std_logic );
+    END COMPONENT;
+
+    COMPONENT Selector IS
+        PORT (sel: IN std_logic;
+              x,y: IN std_logic_vector(15 DOWNTO 0);
+              f: OUT std_logic_vector(15 DOWNTO 0) );
+    END COMPONENT;
+   
+    COMPONENT Reg IS
+        PORT (I: IN std_logic_vector(15 DOWNTO 0);
+              Q: OUT std_logic_vector(15 DOWNTO 0);
+              Ld: IN std_logic; 
+              Clk, Rst: IN std_logic );
+    END COMPONENT;
+    
+    COMPONENT ThreeStateBuff IS
+        PORT (Control_Input: IN std_logic;
+              Data_Input: IN std_logic_vector(15 DOWNTO 0);
+              Output: OUT std_logic_vector(15 DOWNTO 0) );
+    END COMPONENT;
+    
+    COMPONENT Controller IS
+       PORT(R_en: OUT std_logic;
+            W_en: OUT std_logic;
+            R_Addr1: OUT std_logic_vector(2 DOWNTO 0);
+				R_Addr2: OUT std_logic_vector(2 DOWNTO 0);
+            W_Addr: OUT std_logic_vector(2 DOWNTO 0);
+            Shifter_sel: OUT std_logic;
+            Selector_sel: OUT std_logic;
+            ALU_sel : OUT std_logic;
+            OutReg_Ld: OUT std_logic;
+            Oe: OUT std_logic;
+            Done: OUT std_logic;
+            Start, Clk, Rst: IN std_logic); 
+     END COMPONENT;
+-- do not modify any code above this line
+-- add signals needed below. hint: name them something you can keep track of while debugging/testing
+-- add your code starting here
+SIGNAL R_en_out, W_en_out, Shifter_sel_out, Selector_sel_out, ALU_sel_out, OutReg_Ld_out, Oe_out, Done_out: std_logic;
+SIGNAL R_Addr1_out, R_Addr2_out, W_Addr_out: std_logic_vector(2 DOWNTO 0);
+SIGNAL Reg_Data1_out, Reg_Data2_out: std_logic_vector(15 DOWNTO 0);
+SIGNAL ALU_Result: STD_LOGIC_VECTOR (15 DOWNTO 0);
+SIGNAL Shifter_Result: std_logic_vector(15 DOWNTO 0);
+SIGNAL Selector_Result: std_logic_vector(15 DOWNTO 0);
+SIGNAL Reg_Result: std_logic_vector(15 DOWNTO 0);
+SIGNAL Location: std_logic_vector(15 DOWNTO 0);
+
+BEGIN 
+    Controller_Part: Controller PORT MAP
+    (R_en => R_en_out, 
+    W_en => W_en_out, 
+    R_Addr1 => R_Addr1_out, 
+    R_Addr2 => R_Addr2_out, 
+    W_Addr => W_Addr_out, 
+    Shifter_sel => Shifter_sel_out,
+    Selector_sel => Selector_sel_out, 
+    ALU_sel => ALU_sel_out, 
+    OutReg_Ld => OutReg_Ld_out, 
+    Oe => Oe_out, 
+    Done => Done_out, 
+    Start => Start, 
+    Clk => Clk, 
+    Rst => Rst);
+    
+    RegFile_Part: RegFile PORT MAP
+    (R_Addr1 => R_Addr1_out,
+    R_Addr2 => R_Addr2_out, 
+    W_Addr => W_Addr_out,
+    R_en => R_en_out, 
+    W_en => W_en_out, 
+    Reg_Data1 => Reg_Data1_out,
+    Reg_Data2 => Reg_Data2_out,
+    W_Data => Selector_Result,
+    Clk => Clk,
+    Rst => Rst);
+    
+    ALU_Part: ALU PORT MAP
+    (Sel => ALU_sel_out,
+    A => Reg_Data2_out,
+    B => Reg_Data1_out,
+    ALU_Out => ALU_Result);
+        
+    Shifter_Part: Shifter PORT MAP
+    (I => Reg_Data2_out,
+    Q => Shifter_Result,
+    sel => Shifter_sel_out);
+    
+    Selector_Part: Selector PORT MAP
+    (sel => Selector_sel_out,
+    x => Shifter_Result,
+    y => ALU_Result,
+    f => Selector_Result);
+    
+    OutReg_Part: Reg PORT MAP
+    (I => Selector_Result,
+    Q => Reg_Result,
+    Ld => OutReg_Ld_out,
+    Clk => Clk,
+    Rst => Rst);
+    
+    ThreeStateBuff_Part: ThreeStateBuff PORT MAP
+    (Control_Input => Oe_out,
+    Data_Input => Reg_Result,
+    Output => Location);
+    
+    PROCESS (Location, Clk)
+    BEGIN
+            Loc <= Location;
+            Done <= Done_out;        
+    END PROCESS;
+    
+end Struct;
+
---
+++ Lab_3_Accelerating_Object_Locator_Structural/Lab_3_Accelerating_Object_Locator_Structural.srcs/sources_1/imports/Downloads/lab3s_tb.vhd
@@ -0,0 +1,124 @@
+--------------------------------------------------------------------------------
+-- Company: EECS 31L - S'21
+-- Engineer: Shaoxuan Yuan
+--
+-- Create Date: May 24th, 2021
+-- Design Name:   
+-- Module Name:   
+-- Project Name:  lab03
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Locator_struct
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE std.textio.ALL; -- for write, writelin
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY lab3s_tb IS
+END lab3s_tb;
+ 
+ARCHITECTURE behavior OF lab3s_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Locator_struct
+    PORT(
+         Clk : IN  std_logic;
+         Start : IN  std_logic;
+         Rst : IN  std_logic;
+         Loc : OUT  std_logic_vector(15 downto 0);
+		 Done : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Clk : std_logic := '0';
+   signal Start : std_logic := '0';
+   signal Rst : std_logic := '1';
+
+     --Outputs
+   signal Loc : std_logic_vector(15 downto 0);
+   signal Done : std_logic;
+
+   -- Clock period definitions; change to correct value; code will not compile w/o it
+   constant Clk_period : time := 38 ns;
+ 
+BEGIN
+ 
+    -- Instantiate the Unit Under Test (UUT)
+   uut: Locator_struct PORT MAP (
+          Clk => Clk,
+          Start => Start,
+          Rst => Rst,
+		  Loc => Loc,
+          Done => Done
+        );
+
+   -- Clock process definitions
+   Clk_process :process
+   begin
+        Clk <= '0';
+        wait for Clk_period/2;
+        Clk <= '1';
+        wait for Clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+    variable stringbuff : LINE;
+   begin        
+		-- feel free to change any code below. This is not complete, just a template to start with.
+		-- add your assert statement where appropriate based on your FSMD
+		
+        WRITE (stringbuff, string'("Simulation starts at "));
+        WRITE (stringbuff, now);
+        WRITELINE (output, stringbuff);
+
+        Rst <= '1';
+        WAIT FOR 100 NS; -- allow time for everything to rst
+        Rst <= '0';
+        WAIT FOR 15 NS; -- give some time between pressing rst & start button
+        
+        Start <= '1';
+        WAIT UNTIL rising_edge(clk); -- wait for a rising edge so that it is easier to calculate next several wait statements
+        WAIT FOR 10 NS; -- hold for some amount of time
+        Start <= '0';
+        
+        WAIT UNTIL Done = '1';        
+        ASSERT Loc = X"1b50" REPORT "Loc = 1b50 fail with test case 0" SEVERITY WARNING;
+        Rst <= '1';
+        WAIT FOR Clk_period + 1 NS;
+        Rst <= '0';
+        
+        WAIT UNTIL Done = '0';
+        ASSERT Loc = "ZZZZ" REPORT "Loc = ZZZZ fail with test case 1" SEVERITY WARNING;
+
+        WRITE (stringbuff, string'("Simulation Ends at "));
+        WRITE (stringbuff, now);
+        WRITELINE (output, stringbuff);
+
+        wait; -- will wait forever
+   end process;
+
+END;
